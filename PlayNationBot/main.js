44eb242ee2032a069f9c1906bbe4ec79e5cc9959
AnhMTV <maithachvietanh@gmail.com>
2024-04-15 16:10:46 +0700

    Update bot with env

--- PlayNationBot/main.js
+++ PlayNationBot/main.js
@@ -1,21 +1,26 @@
-
 const TelegramBot = require('node-telegram-bot-api');
+require('dotenv').config();
 
-// replace the value below with the Telegram token you receive from @BotFather
-const token = 'YOUR_TELEGRAM_BOT_TOKEN';
-
-// Create a bot that uses 'polling' to fetch new updates
+const token = process.env.TELEGRAM_BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
 // Matches "/start" command
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
+  // const opts = {
+  //   reply_markup: JSON.stringify({
+  //     inline_keyboard: [
+  //       [{ text: 'Play game', callback_data: 'play_game' }],
+  //       [{ text: 'Earn Fish', callback_data: 'earn_fish' }]
+  //     ]
+  //   })
+  // };
   const opts = {
     reply_markup: JSON.stringify({
-      inline_keyboard: [
-        [{ text: 'Play game', callback_data: 'play_game' }],
-        [{ text: 'Earn Fish', callback_data: 'earn_fish' }]
-      ]
+      keyboard: [
+        [{ text: 'Play game' }],
+        [{ text: 'Get more point' }],
+      ],
     })
   };
   bot.sendMessage(chatId, 'Choose an option:', opts);
@@ -28,9 +33,9 @@ bot.on('callback_query', (callbackQuery) => {
 
   let text;
   if (category === 'play_game') {
-    text = 'Let’s play a game!';
+    text = 'https://t.me/BookaGamesBot/swbooka?startapp=EXvDA66or';
   } else if (category === 'earn_fish') {
-    text = 'Time to earn some fish!';
+    text = 'https://t.me/BookaGamesBot/swbooka?startapp=EXvDA66or';
   }
 
   bot.sendMessage(message.chat.id, text);
