b4a1468a87c43fb55a1a420145595cd32406ac7f
anhnhu <anhnt@subwallet.app>
2024-04-15 17:18:19 +0700

    Update start command

--- PlayNationBot/main.js
+++ PlayNationBot/main.js
@@ -1,42 +1,27 @@
-const TelegramBot = require('node-telegram-bot-api');
+const {Markup} = require("telegraf");
+const Telegraf = require('telegraf').Telegraf;
 require('dotenv').config();
-
-const token = process.env.TELEGRAM_BOT_TOKEN;
-const bot = new TelegramBot(token, { polling: true });
-
-// Matches "/start" command
-bot.onText(/\/start/, (msg) => {
-  const chatId = msg.chat.id;
-  // const opts = {
-  //   reply_markup: JSON.stringify({
-  //     inline_keyboard: [
-  //       [{ text: 'Play game', callback_data: 'play_game' }],
-  //       [{ text: 'Earn Fish', callback_data: 'earn_fish' }]
-  //     ]
-  //   })
-  // };
-  const opts = {
-    reply_markup: JSON.stringify({
-      keyboard: [
-        [{ text: 'Play game' }],
-        [{ text: 'Get more point' }],
-      ],
-    })
-  };
-  bot.sendMessage(chatId, 'Choose an option:', opts);
-});
-
-// Handle callback queries
-bot.on('callback_query', (callbackQuery) => {
-  const message = callbackQuery.message;
-  const category = callbackQuery.data;
-
-  let text;
-  if (category === 'play_game') {
-    text = 'https://t.me/BookaGamesBot/swbooka?startapp=EXvDA66or';
-  } else if (category === 'earn_fish') {
-    text = 'https://t.me/BookaGamesBot/swbooka?startapp=EXvDA66or';
-  }
-
-  bot.sendMessage(message.chat.id, text);
-});
+const {BANNER_URL, WEB_APP_URL, TELEGRAM_BOT_TOKEN} = process.env;
+const bot = new Telegraf(TELEGRAM_BOT_TOKEN);
+bot.telegram.setMyCommands([
+    {
+        command: 'start',
+        description: 'Start App',
+    },
+]);
+bot.command("start", async ctx => {
+        await ctx.reply('Welcome!', Markup.keyboard([Markup.button.webApp('Open apps', WEB_APP_URL)], ).resize(true));
+        await ctx.replyWithPhoto(
+            BANNER_URL,
+            {
+                caption:
+                    'Playnation is a Telegram-based gaming wallet featuring a wide array of highly interactive games, tailored to connect Web3 users with abundant airdrops from top-tier web3 projects' ,
+                parse_mode: 'HTML',
+                reply_markup: {
+                    inline_keyboard: [[Markup.button.webApp('Open apps', WEB_APP_URL)]],
+                }
+            }
+        );
+    }
+);
+bot.launch();
